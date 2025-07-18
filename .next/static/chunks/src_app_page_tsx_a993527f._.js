41dd0792d66e190e0e4334807ac9828b7747f5ab
gaming4668 <helloacchello2@gmail.com>
2025-07-18 16:19:19 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752835812743

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1e19690a._.js",
+  "static/chunks/node_modules_ddf7d5a1._.js"
+],
+    source: "dynamic"
+});
